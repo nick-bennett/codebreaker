9263ae661e42da18945317098ffdc6c815fc235f
Nick Bennett <nick@nickbenn.com>
2020-09-24 16:34:48 -0600

    Implemented Game class.

--- src/edu/cnm/deepdive/codebreaker/model/Code.java
+++ src/edu/cnm/deepdive/codebreaker/model/Code.java
@@ -1,6 +1,13 @@
 package edu.cnm.deepdive.codebreaker.model;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 public class Code {
 
@@ -16,6 +23,80 @@ public class Code {
   @Override
   public String toString() {
     return new String(secret);
+  }
+
+  public class Guess {
+
+    private static final String STRING_FORMAT = "{text: \"%s\", correct: %d, close: %d}";
+
+    private final String text;
+    private final int correct;
+    private final int close;
+
+    public Guess(String text) {
+      this.text = text;
+      int correct = 0;
+      int close = 0;
+
+      Map<Character, Set<Integer>> letterMap = getLetterMap(text);
+
+      char[] work = Arrays.copyOf(secret, secret.length);
+
+      for (int i = 0; i < work.length; i++) {
+        char letter = work[i];
+        Set<Integer> positions = letterMap.getOrDefault(letter, Collections.emptySet());
+        if (positions.contains(i)) {
+          correct++;
+          positions.remove(i);
+          work[i] = 0;
+        }
+      }
+
+      for (char letter : work) {
+        if (letter != 0) {
+          Set<Integer> positions = letterMap.getOrDefault(letter, Collections.emptySet());
+          if (positions.size() > 0) {
+            close++;
+            Iterator<Integer> iter = positions.iterator();
+            iter.next();
+            iter.remove();
+          }
+        }
+      }
+
+      this.correct = correct;
+      this.close = close;
+    }
+
+    private Map<Character, Set<Integer>> getLetterMap(String text) {
+      Map<Character, Set<Integer>> letterMap = new HashMap<>();
+      char[] letters = text.toCharArray();
+      for (int i = 0; i < letters.length; i++) {
+        char letter = letters[i];
+        Set<Integer> positions = letterMap.getOrDefault(letter, new HashSet<>());
+        positions.add(i);
+        letterMap.putIfAbsent(letter, positions);
+      }
+      return letterMap;
+    }
+
+    @Override
+    public String toString() {
+      return String.format(STRING_FORMAT, text, correct, close);
+    }
+
+    public String getText() {
+      return text;
+    }
+
+    public int getCorrect() {
+      return correct;
+    }
+
+    public int getClose() {
+      return close;
+    }
+    
   }
 
 }
